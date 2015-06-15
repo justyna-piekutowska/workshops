1a5bbc893e000418785ca6100d259e522b8cbe51
Justyna Piekutowska <piekutowska.justyna@gmail.com>
2015-06-15 23:32:00 +0200

    Fix sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,15 @@
 class ApplicationController < ActionController::Base
-
+  before_action :configure_permitted_parameters, if: :devise_controller?
   decent_configuration do
     strategy DecentExposure::StrongParametersStrategy
   end
 
   protect_from_forgery with: :exception
+
+  protected
+    def configure_permitted_parameters
+      sign_up_params = devise_parameter_sanitizer.for(:sign_up)
+      sign_up_params << :firstname
+      sign_up_params << :lastname
+    end
 end
